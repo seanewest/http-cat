99b242cb87c6b106bffac09914fe8187642087cc
Sean West <sean.e.west@gmail.com>
2014-12-23 02:20:30 -0500

    httpcat

---
+++ cmd.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+var http = require('http');
+var concat = require('concat-stream');
+
+var port = process.argv[2] || 2000;
+
+process.stdin.pipe(concat(function(data) {
+  http.createServer(function(req, res) {
+    res.end(data);
+  }).listen(port)
+  console.log('listening on port ' + port);
+}))
---
+++ readme.md
@@ -0,0 +1,18 @@
+# HTTPCat
+
+Pipe anything directly to an http server at a specific port.
+
+```
+echo "<h1>hello</h1>" | httpcat 2000
+```
+
+```
+curl localhost:2000
+<h1>hello</h1>
+```
+
+## Install
+
+```
+npm install seanewest/httpcat
+```
